f427b29d4d16ac4820b4a4d5708eb1d1e08a91ab
hajrajaa <129968202+hajrajaa@users.noreply.github.com>
2024-12-10 12:30:35 +0200

    new_update

--- HW2_Ngrams/knesset_language_models.py
+++ HW2_Ngrams/knesset_language_models.py
@@ -21,7 +21,7 @@ class Trigram_LM:
        self.total_toknes=0
 
        # define interpolation weights 
-       self.lamda1,self.lamda2,self.lamda3= 0.5,0.3,0.2
+       self.lamda1,self.lamda2,self.lamda3= 0.7,0.1,0.2
 
        self._build_model(corpus)
 
@@ -30,42 +30,46 @@ class Trigram_LM:
 
     def _build_model(self,corpus):
 
-        for sentence in corpus:
+        for protocol_name,sentences in corpus.items():
 
-            toknes=sentence.split()
-            ## add dummy toknes to the sentence
-            toknes.insert(0,"<s_0>")
-            toknes.insert(1,"<s_1>")
+            for sentence in sentences:
 
-            self.total_toknes+= len(toknes)
+                toknes=sentence.split()
+                ## add dummy toknes to the sentence
+                ## check if need to add space after each dummy toknes 
+                toknes.insert(0,"<s_0>")
+                toknes.insert(1,"<s_1>")
 
-            for i in range(len(toknes)):
+                self.total_toknes+= len(toknes)
 
-                self.unigram_counts[toknes[i]]+=1
+                for i in range(len(toknes)):
 
-                if i>0:
-                    self.bigram_counts[toknes[i-1]][toknes[i]]+=1
+                    self.unigram_counts[toknes[i]]+=1
 
-                if i>1:
-                    self.trigram_counts[(toknes[i-2],toknes[i-1])][toknes[i]]+=1
+                    if i>0:
+                        self.bigram_counts[toknes[i-1]][toknes[i]]+=1
+
+                    if i>1:
+                        self.trigram_counts[(toknes[i-2],toknes[i-1])][toknes[i]]+=1
 
 
     # section 1.1 
     def calculate_prob_of_sentence(self,sentence):
 
-        toknes= sentence.split()
+        # checkkkk !!!!!
+        toknes=sentence.split()
 
         ## add dummy toknes to the sentence
         toknes.insert(0,"<s_0>")
         toknes.insert(1,"<s_1>")
-        print (toknes)
-        #toknes.append("<s_end>")
+      
 
         log_prob=0.0
         size=len(self.unigram_counts)
 
 
         for i in range(2,len(toknes)):
+
 
             # get the current trigrams
             w1,w2,w3 =toknes[i-2] ,toknes[i-1] ,toknes[i]
@@ -94,20 +98,33 @@ class Trigram_LM:
     
     # section 1.2 
     def  generate_next_token(self,sentence):
+        
 
-        w1,w2=sentence.split()[-2:]
 
         vocab_size=len(self.unigram_counts)
         probs=defaultdict(float)
+
+        #w1,w2=sentence.split()[-2],sentence.split()[-1]
+        toknes=sentence.split()
+        
+        if len(toknes)<2:
+            toknes.insert(0,"<s_1>")
+        if len(toknes)<1 or toknes[0]!="<s_0>":
+            toknes.insert(0,"<s_0>")
+
+        w1,w2=toknes[-2],toknes[-1]
 
         for token in self.unigram_counts:
 
             # skip the dummy toknes 
             if token in ["<s_0>","<s_1>"]:
                 continue
+            
+            #print(w1,w2)
 
              # calculate the trigram probability
             tri_count= self.trigram_counts[(w1,w2)].get(token,0)
+            
             total_tri= sum(self.trigram_counts[(w1,w2)].values())
             tri_prob= (tri_count +1)/(total_tri+vocab_size)
 
@@ -123,26 +140,26 @@ class Trigram_LM:
             # apply linear interpolation 
             probs[token]= self.lamda1* tri_prob + self.lamda2*bi_prob + self.lamda3*uni_prob
 
-            next_token=max(probs,key=probs.get)
-            return next_token,probs[next_token]
+        next_token=max(probs,key=probs.get)
+        return next_token,probs[next_token]
 
 
 
 # %% section 2.1 
 # check if correct 
-def  get_k_n_t_collocation(self,k,n,t,metric="frequency"):
+def  get_k_n_t_collocation(corpus,k,n,t,metric="frequency"):
 
     try:
         ngram_counts=Counter()
         protocol_count=defaultdict(set)
-        total_protocol=len(self.corpus)
+        total_protocol=len(corpus)
         
-        for protocol_name,sentences in self.corpus.items():
+        for protocol_name,sentences in corpus.items():
 
             for sentence in sentences:
                 toknes=sentence.split()
                 if len(toknes)<n:
-                     continue 
+                    continue 
                 ngrams=zip(*[toknes[i:] for i in range(n)])
 
                 for ngram in ngrams:
@@ -180,15 +197,7 @@ def  get_k_n_t_collocation(self,k,n,t,metric="frequency"):
 
     except Exception as e:
         raise e
-       
-
-
-
-
-
-
-
-
+    
         
 
 
@@ -225,9 +234,9 @@ def save_collocations(plenary_corpus,committee_corpus,output_file):
 
             f_out.write("Committee corpus:\n")
             committe_collocations_freq=get_k_n_t_collocation(committee_corpus,k=10,n=2,t=5,metric="frequency")
-            print(committe_collocations_freq)
+            #print(committe_collocations_freq)
             for ngram in committe_collocations_freq:
-                print(ngram[0])
+                #print(ngram[0])
                 f_out.write(f"{ngram[0]} \n")
             f_out.write("\n")
             
@@ -337,7 +346,7 @@ def mask_toknes_in_sentences(sentences,x):
         mask_indices=random.sample(range(tokens_len),maske_len)
 
         for i in mask_indices:
-            toknes[i]='*'
+            toknes[i]='[*]'
         masked_sentences.append(" ".join(toknes))
 
     return masked_sentences 
@@ -347,6 +356,7 @@ def save_masked_sentences(corpus):
 
     orignal_output_file='original_sampled_sents.txt'
     masked_output_file='masked_sampled_sents.txt'
+    masked_sentences=[]
 
     try:
 
@@ -360,6 +370,7 @@ def save_masked_sentences(corpus):
                 f_out.write(f"{sentence}\n")
 
         masked_sentences=mask_toknes_in_sentences(sentences,10)
+       
         if not masked_sentences:
             raise ValueError("Empty masked sentenced")
         
@@ -367,15 +378,88 @@ def save_masked_sentences(corpus):
         with open(masked_output_file,'w',encoding='utf-8') as f_out:
              for sentence in masked_sentences:
                  f_out.write(f"{sentence}\n")
-
         
-
-
+        return sentences,masked_sentences
 
 
     except Exception as e:
         raise e 
+    
 
+## section 3.3 
+
+def predict_masked_toknes(lm,masked_sentence):
+
+
+    predicted_sentence=[]
+    predicted_toknes=[]
+
+    curr_toknes=masked_sentence.split()
+
+    masked_indices=[i for i in range(len(curr_toknes))if curr_toknes[i]=='[*]']
+    
+
+    for i in masked_indices:
+            #curr_text=" ".join(curr_toknes[max(0,i-2):i])
+            #curr_text=" ".join(curr_toknes[:i])+" <MASK>"
+            #print(curr_toknes[:i])
+            curr_text=" ".join(curr_toknes[:i])
+            #print(curr_text)
+            next_token,prob=lm.generate_next_token(curr_text)
+            curr_toknes[i]=next_token
+            predicted_toknes.append(next_token)
+        
+    predicted_sentence.append(" ".join(curr_toknes))
+    
+
+    return " ".join(predicted_sentence),predicted_toknes
+
+
+
+
+ 
+
+      
+
+def save_sampled_sentences(plenary_ml,committee_ml,output_file,orignal_sentences,masked_sentences):
+
+    try:
+
+        with open(output_file,'w',encoding='utf-8') as f_out:
+
+            for i in range(len(orignal_sentences)):
+                f_out.write("original_sentence: ")
+                f_out.write(f"{orignal_sentences[i]}\n")
+                orignal_prob=plenary_ml.calculate_prob_of_sentence(orignal_sentences[i])
+                print(orignal_prob)
+                f_out.write("masked_sentence: ")
+                f_out.write(f"{masked_sentences[i]}\n")
+                f_out.write("plenary_sentence: ")
+
+                predicted_sentence,predicted_toknes=predict_masked_toknes(plenary_ml,masked_sentences[i])
+
+                f_out.write(f"{predicted_sentence}\n")
+                predicted_toknes=",".join(predicted_toknes)
+                f_out.write(f"plenary_tokens: {predicted_toknes} \n")
+                
+
+                ## correct this 
+                # for token in predicted_toknes:
+                #     f_out.write(f"{token}")
+                #     if token!=predicted_toknes[-1]:
+                #         f_out.write(",")
+                #     else:
+                #         f_out.write("\n")
+                
+                plenary_prob=plenary_ml.calculate_prob_of_sentence(predicted_sentence)
+                committee_prob=committee_ml.calculate_prob_of_sentence(predicted_sentence)
+                f_out.write(f"probability of plenary sentence in plenary corpus: {plenary_prob:.2f}\n")
+                f_out.write(f"probability of plenary sentence in committee corpus: {committee_prob:.2f}\n")
+
+
+
+    except Exception as e:
+        raise e
 
 
 
@@ -423,43 +507,24 @@ if __name__=='__main__':
 
         
         ## section 2 
-        save_collocations(plenary_model,committee_model,output_file)
+        #save_collocations(plenary_model.corpus,committee_model.corpus,output_file)
 
         ## section 3 
-        save_masked_sentences(committee_corpus)
+        orignal_sentences,masked_sentences=save_masked_sentences(committee_corpus)
+
+        ## section 3.3 
+        results_file='sampled_sents_results.txt'
+        save_sampled_sentences(plenary_model,committee_model,results_file,orignal_sentences,masked_sentences)
     
     except Exception as e:
         raise e
+    
+    
 
 
 
 
 
 
-# %% Main 
-
-
-### for check section one 
-# corpus = [
-#     "hello world",
-#     "hello there",
-#     "world of hello",
-#     "there is a world"
-# ]
-
-# def test_calculate_prob_of_sentence():
-#     lm = Trigram_LM(corpus)
-#     sentence = "hello world"
-#     prob = lm.calculate_prob_of_sentence(sentence)
-#     print(f"Log probability of '{sentence}': {prob}")
-
-# def test_generate_next_token():
-#     lm = Trigram_LM(corpus)
-#     context = "hello world"
-#     next_token, prob = lm.generate_next_token(context)
-#     print(f"Next token after '{context}': {next_token}, Probability: {prob}")
-
-# test_calculate_prob_of_sentence()
-# test_generate_next_token()
 
 # %%
