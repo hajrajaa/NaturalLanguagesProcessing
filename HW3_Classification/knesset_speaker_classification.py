34036ba75e518751a38501881029964d4b635580
hajrajaa <129968202+hajrajaa@users.noreply.github.com>
2025-01-07 16:21:10 +0200

    done !!

--- HW3_Classification/knesset_speaker_classification.py
+++ HW3_Classification/knesset_speaker_classification.py
@@ -22,13 +22,14 @@ class speakerData:
     def __init__(self,name):
         self.name=name
         self.sentences=[]    #list to store all the sentences of the speaker
-        self.sentences_count=0
+        self.sentences_count=0   # number of sentences of the speaker       
     
     def add_sentence(self,sentence):
         self.sentences.append(sentence)
-        self.sentences_count+=1
+        self.sentences_count+=1   # update the number of sentences 
 ###########################################################################################################################################
 
+# Load the corpus
 def load_corpus(file_path):
 
     corpus=[]
@@ -36,6 +37,7 @@ def load_corpus(file_path):
         with open(file_path ,'r', encoding='utf-8') as f:
 
             for sentence in f:
+                # get the data from the json file 
                 data=json.loads(sentence)
                 corpus.append(data)
 
@@ -52,6 +54,7 @@ def load_sentences(file_path):
     try:
         with open(file_path,'r',encoding='utf-8') as f:
             for line in f:
+                
                 sentences.append(line.strip())
     except Exception as e:
         raise e
@@ -61,23 +64,24 @@ def load_sentences(file_path):
 
 ##########################################################################################################################################
 ## scection 1
-#  get the most common speakers
+#  get the most common speakers in the corpus 
 def get_common_speakers(corpus):
     
+    # counter for the speakers 
     speaker_counter=Counter()
+
     for data in corpus:
 
         speaker_name=data.get("speaker_name",None)
         if speaker_name:
             speaker_counter[speaker_name]+=1
 
-    # get the most common speakers
+    # get the most 2 common speakers
     most_common_speakers=speaker_counter.most_common(2)
+
+    #get the names of the speakers 
     first_speaker_name=most_common_speakers[0][0] if len(most_common_speakers)>0 else None
     secound_speaker_name=most_common_speakers[1][0] if len(most_common_speakers)>1 else None
-
-    # print(f"First Speaker: {first_speaker_name}")
-    # print(f"Secound Speaker: {secound_speaker_name}")
 
     return first_speaker_name,secound_speaker_name 
 
@@ -95,56 +99,58 @@ def split_data(first_speaker,secound_speaker,other_speakers,corpus):
         
         curr_name=data.get("speaker_name",None)
 
-    
+        # in case of the curr name is the first speaker name or is an alias of the first_speaker name 
         if curr_name in aliases.get(first_speaker.name,set()) or curr_name==first_speaker.name:
+            ## add the sentence to the first speaker class 
             first_speaker.add_sentence(data)
 
+        ## in case of the curr name is the second speaker name or is an alias of the first_speaker name 
         elif curr_name in aliases.get(secound_speaker.name,set()) or curr_name==secound_speaker.name :
+            # add the sentence to the second speaker class 
             secound_speaker.add_sentence(data)
-      
+
         else:
+            # dd the sentence to the other speaker class
             other_speakers.add_sentence(data)
 
     return first_speaker,secound_speaker,other_speakers
+
 ##############################################################################################################################################################
 ## section 2
 # down sample the data 
 def down_sample(first_speaker,secound_speaker):
 
+    # get the size of the speakers classes 
     first_speaker_size=first_speaker.sentences_count
     secound_speaker_size=secound_speaker.sentences_count
 
-
+    # in case the size of the first speaker grather than the secound speaker 
     if first_speaker_size>secound_speaker_size:
         first_speaker.sentences=random.sample(first_speaker.sentences,secound_speaker_size)
+        # update the size field
         first_speaker.sentences_count=secound_speaker_size
+
+    # in case the size of the second speaker grather than the first speaker     
     elif first_speaker_size<secound_speaker_size:
         secound_speaker.sentences=random.sample(secound_speaker.sentences,first_speaker_size)
-        secound_speaker.sentences_count=first_speaker_size
+        # update the size field
+        secound_speaker.sentences_count=first_speaker_size   
 
     return first_speaker,secound_speaker
 
 
 def run_down_sampling(first_speaker,secound_speaker,other_speakers):
         
-        # print("DOWN SAMPLING THE DATA:\n")
-        # print("size before down sampling:\n")
-        # print(f"Size of the First Speaker:{first_speaker.sentences_count}\n")
-        # print(f"Size of the Secound Speaker:{secound_speaker.sentences_count}\n")
-        # print(f"Size of the Other speakers:{other_speakers.sentences_count}\n")
+      
 
         first_speaker,secound_speaker=down_sample(first_speaker,secound_speaker)
         
         first_speaker,other_spekaers=down_sample(first_speaker,other_speakers)
 
+        # to make sure all the classes have the same number of sentences 
         if other_spekaers.sentences_count!=secound_speaker.sentences_count:
-            secound_speaker=down_sample(secound_speaker,other_spekaers)
+            secound_speaker,other_spekaers=down_sample(secound_speaker,other_spekaers)
 
-        # print("*****************************************")
-        # print("size after down sampling:\n")
-        # print(f"Size of the First Speaker:{first_speaker.sentences_count}\n")
-        # print(f"Size of the Secound Speaker:{secound_speaker.sentences_count}\n")
-        # print(f"Size of the Other speakers:{other_speakers.sentences_count}\n")
 
         return first_speaker,secound_speaker,other_speakers
 
@@ -161,13 +167,15 @@ def extract_BoW_vector(vectorizer=None,sentences=None):
 
     return bow_vectors
 
-
+# get the unique words for the speakers 
 def get_unique_words(first_speaker,secound_speaker,other_speakers):
-                     
+
+    # counter for the words                  
     first_words_count=Counter()
     secound_words_count=Counter()
     other_words_count=Counter()
 
+    
     for sentence in first_speaker.sentences:
         words=sentence.get('sentence_text','').split()
         first_words_count.update(words)
@@ -181,7 +189,9 @@ def get_unique_words(first_speaker,secound_speaker,other_speakers):
         other_words_count.update(words)
 
     unique_words=set()
+
     for word,count in first_words_count.items():
+        
         if count >= 250 and count >= 2*max(secound_words_count[word],other_words_count[word]):
             unique_words.add(word)
 
@@ -193,6 +203,7 @@ def get_unique_words(first_speaker,secound_speaker,other_speakers):
         if count >= 250 and count >=2* max(first_words_count[word],secound_words_count[word]):
             unique_words.add(word)
 
+    # remove the punctuations from the unique words 
     to_exlude=['\'','"','!','(',')','.',',',';','?','-','%',':','–']
     unique_words=[word for word in unique_words if word not in to_exlude]
     
@@ -200,9 +211,7 @@ def get_unique_words(first_speaker,secound_speaker,other_speakers):
     
 
 # section 3:2 - Extract feature vector 
-def extract_feature_vector(sentences):
-
-    global special_words
+def extract_feature_vector(sentences,special_words):
 
     features=[]
   
@@ -278,54 +287,41 @@ def train_model(features,labels):
 
     return knn_model,knn_cv_scores,lr_model,lr_cv_scores
 
-def run_training(sentences,labels,classification_type='Multi-Class') :
+def run_training(sentences,labels,special_words,classification_type='Multi-Class') :
     try:
 
         
         sentences_txt=[sentence.get('sentence_text','') for sentence in sentences]
 
+        # extract the Bow vector with CountVectorizer 
         vectorizer_count=CountVectorizer()
         bow_count_vector=extract_BoW_vector(vectorizer_count,sentences_txt)
         
-        
+        # extract the Bow vector with TfidfVectorizer 
         vectorizer_tf=TfidfVectorizer()
         bow_vector=extract_BoW_vector(vectorizer_tf,sentences_txt)
-        features_vector=extract_feature_vector(sentences)
 
-        
-    
-        #print(f"{classification_type} Classification:")
+        #extract the custom feature vector 
+        features_vector=extract_feature_vector(sentences,special_words)
+
+        # dictionary to store the classification report 
         combind_report={}
 
-
-        #print("CountVectorizer:\n")
+        # train the models 
         count_knn_model,bow_count_knn,count_lr_model,bow_count_lr=train_model(bow_count_vector,labels)
         combind_report['CountVectorizer_KNN']=classification_report(labels,count_knn_model.predict(bow_count_vector), output_dict=True)
         combind_report['CountVectorizer_LR']=classification_report(labels,count_lr_model.predict(bow_count_vector), output_dict=True)
-        # print("BoW classifier score -KNN:\n",bow_count_knn)
-        # print("BoW classifier score mean-KNN:",bow_count_knn.mean())
-        # print("BoW classifier score -LR:\n",bow_count_lr)
-        # print("BoW classifier score mean-LR:",bow_count_lr.mean())
-
-        # print("******************************************************************")
-        # print("TfidfVectorizer:\n")
+     
         knn_model,bow_knn,lr_model,bow_lr=train_model(bow_vector,labels)
         combind_report['TfidfVectorizer_KNN']=classification_report(labels,knn_model.predict(bow_vector), output_dict=True)
         combind_report['TfidfVectorizer_LR']=classification_report(labels,lr_model.predict(bow_vector), output_dict=True)
-        # print("BoW Classifier Score -KNN:\n",bow_knn)
-        # print("BoW classifier score mean-KNN:",bow_knn.mean())
-        # print("BoW Classifier Score-LR:\n",bow_lr)
-        # print("BoW classifier score mean-LR:",bow_lr.mean())
+        
 
-        # print("******************************************************************")
-        # print("Custom Feature Vector:\n")
+       
         custom_knn_model,feature_knn,custom_lr_model,feature_lr=train_model(features_vector,labels)
         combind_report['CustomFeatureVector_KNN']=classification_report(labels,custom_knn_model.predict(features_vector), output_dict=True)
         combind_report['CustomFeatureVector_KNN_LR']=classification_report(labels,custom_lr_model.predict(features_vector), output_dict=True)
-        # print("Custom Feature Classifier Score -KNN:\n",feature_knn)
-        # print("Custom Feature Classifier Score mean -KNN: ",feature_knn.mean())
-        # print("Custom Feature Classifier Score -LR: \n",feature_lr)
-        # print("Custom Feature Classifier Score mean -LR: ",feature_lr.mean())
+        
 
         # print("\nClassification Report:\n")
         # for model,report in combind_report.items():
@@ -342,51 +338,61 @@ def run_training(sentences,labels,classification_type='Multi-Class') :
     
 #############################################################################################################################
 # Section 5 - classify the speakers 
-def classify_speaker(sentences,output_fir,model,vectorizer=None):
+def classify_speaker(sentences,output_dir,model,vectorizer=None):
 
     try:
-        output_path=os.path.join(output_dir,'classification_results.txt')
+        output_path=os.path.join(output_dir,"classification_results.txt")
         with open(output_path,'w',encoding='utf-8')as f:
-
+            
+            
             sentences_features=vectorizer.transform(sentences)
             
             predictions=model.predict(sentences_features)
            
-            for pred in predictions:
+            # write the predictions to the output file 
+            for pred in predictions[:-1]:
                 f.write(f"{pred}\n")
 
+            f.write(predictions[:-1])
 
     except Exception as e:
         raise e
 #############################################################################################################################
 
-def run(input_path,sentences_texts_file,output_dir):
-     
-    # load the corpus
+
+def run_main(input_path,sentences_texts_file,output_dir):
+
+     # load the corpus
     corpus=load_corpus(input_path) 
 
     # get the most common speakers 
     first_speaker_name,secound_speaker_name=get_common_speakers(corpus)
 
+    first_speaker=speakerData(first_speaker_name)
+    secound_speaker=speakerData(secound_speaker_name)
+    other_speakers=speakerData("other")
+
     # section 1 - split the data 
     first_speaker,secound_speaker,other_speakers=split_data(first_speaker,secound_speaker,other_speakers,corpus)
+
+    # section 3- down sample the data 
+    first_speaker,secound_speaker,other_speakers=run_down_sampling(first_speaker,secound_speaker,other_speakers)
 
     total_sentences=first_speaker.sentences+secound_speaker.sentences+other_speakers.sentences
     binary_sentences=first_speaker.sentences+secound_speaker.sentences
 
     # get the unique words 
     special_words= get_unique_words(first_speaker,secound_speaker,other_speakers)
-    # print(f"Number of unique words: {len(special_words)}")
-    # print(special_words)
+    
 
     # labels for the binary and multu-class classification 
-    labels_binary=["first"]*first_speaker.sentences_count+["secound"]*secound_speaker.sentences_count
+    labels_binary=["first"]*first_speaker.sentences_count+["second"]*secound_speaker.sentences_count
     labels_binary=np.array(labels_binary)
     
 
     labels_multi=(
         ["first"]*first_speaker.sentences_count+
-        ["secound"]*secound_speaker.sentences_count+
+        ["second"]*secound_speaker.sentences_count+
         ["other"]*other_speakers.sentences_count
 
 
@@ -395,8 +401,8 @@ def run(input_path,sentences_texts_file,output_dir):
 
 
     # section 4 - train the model
-    knn_model,custom_knn_model,lr_model,custom_lr_model,vectorizer_tf=run_training(binary_sentences,labels_binary,'Binary')
-    knn_model,custom_knn_model,lr_model,custom_lr_model,vectorizer_tf=run_training(total_sentences,labels_multi,'Multi-Class')
+    knn_model_b,custom_knn_model_b,lr_model_b,custom_lr_b_model,vectorizer_tf_b=run_training(binary_sentences,labels_binary,special_words,'Binary')
+    knn_model,custom_knn_model,lr_model,custom_lr_model,vectorizer_tf=run_training(total_sentences,labels_multi,special_words,'Multi-Class')
 
     
     # section 5 - classify the speaker 
@@ -404,6 +410,10 @@ def run(input_path,sentences_texts_file,output_dir):
     #classify_speaker(test_sentences,'classification_results_lr.txt',lr_model,vectorizer_tf)
     classify_speaker(test_sentences,output_dir,knn_model,vectorizer_tf)
 
+
+    
+     
+   
 
 
 # %% Main 
@@ -429,12 +439,14 @@ if __name__=='__main__':
         if not os.path.exists(output_dir):
             raise FileNotFoundError(f"File Not Found:{output_dir}")
         
-        run(input_path,sentences_texts_file,output_dir)
+        run_main(input_path,sentences_texts_file,output_dir) 
         
         
    
     except Exception as e:
         raise e
+    
+
 
 
 
