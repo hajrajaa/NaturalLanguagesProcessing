f097f1a5431dad53f6597e650b28c6759f56e123
hajrajaa <129968202+hajrajaa@users.noreply.github.com>
2025-01-13 15:20:50 +0200

    section 2:3 finished ( get the similar sentences)

--- HW4_WordEmbeddings/knesset_word2vec.py
+++ HW4_WordEmbeddings/knesset_word2vec.py
@@ -8,45 +8,36 @@ import json
 import numpy as np
 import pandas as pd 
 from gensim.models import Word2Vec
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 
-# section 1:1 - Tokenization 
+# section 1:1 - Tokenization  (dont forget to rerwite the function )
 def tokenize_sentences(sentences):
 
-    tokenized_sentences=[]
+    tokenized_sentences = {}
     
     try:
-
         for sentence in sentences:
-
-           
-            sentence=re.sub(r"[-_]"," ",sentence)
-            curr_toknes=re.sub(r"[!().:;,?%!]","",sentence).split()
+            original_sentence = sentence
+            sentence = re.sub(r"[-_]", " ", sentence)
+            curr_tokens = re.sub(r"[!().:;,?%!]", "", sentence).split()
             
-            filtered_toknes=[]
-            for token in curr_toknes:
-
-                token=token.strip()
-
-                if len(token)<2 :
+            filtered_tokens = []
+            for token in curr_tokens:
+                token = token.strip()
+                if len(token) < 2:
                     continue
-                
                 elif any(char.isdigit() for char in token):
                     continue
+                filtered_tokens.append(token)
 
-                filtered_toknes.append(token)
-
-
-            #print(filtered_toknes)
-            tokenized_sentences.append(filtered_toknes)
-
+            tokenized_sentences[original_sentence] = filtered_tokens
 
     except Exception as e:
         raise e
     
     return tokenized_sentences
-
 
 
 # Load the corpus
@@ -69,7 +60,7 @@ def load_corpus(file_path):
     
     return corpus
 
-
+# section 2:1 
 def get_similar_words(words_list,word_vectors,output_dir='output'):
 
     similar_words={}
@@ -78,7 +69,7 @@ def get_similar_words(words_list,word_vectors,output_dir='output'):
         #output_path=os.path.join(output_dir,"knesset_similar_words.txt")
         with open("knesset_similar_words.txt",'w',encoding='utf-8') as file:
             for word in words_list:
-                print(word)
+                #print(word)
                 similarity_scores={other_word: word_vectors.similarity(word,other_word) for other_word in word_vectors.key_to_index if other_word!=word }
                 
                 sorted_similarty_scores=sorted(similarity_scores.items(),key=lambda item: item[1], reverse=True)
@@ -97,6 +88,57 @@ def get_similar_words(words_list,word_vectors,output_dir='output'):
     
     return similar_words
 
+# section 2:2 - sentnce embeddings 
+def get_sentence_embeddings(sentences,word_vectors):
+
+    sentences_embeddings=[]
+
+    for sentence in sentences:
+        #sentence=sentence.split()
+        sentence_vector=np.zeros(word_vectors.vector_size)
+        k=0   # number of words in the sentence 
+        for token in sentence:
+            # to ensure that the token is in the word vectors
+            if token in word_vectors.key_to_index.keys():
+                sentence_vector+=word_vectors[token]
+                k+=1
+
+            if k>0 :
+                sentence_vector/=k
+        sentences_embeddings.append(sentence_vector)  
+    return sentences_embeddings              
+
+
+
+# section 2:3 - get similar sentences
+def get_similar_sentences(choosen_sentneces,sentences_embeddings,word_vectors,toknes,output_dir):
+
+    #output_path=os.path.join(output_dir,'knesset_similar_sentences.txt')
+    try:
+        tokenized_sentences=[tokenized for _,tokenized in choosen_sentneces]
+        chosen_embeddings=get_sentence_embeddings(tokenized_sentences,word_vectors)
+        similarty_matrix=cosine_similarity(chosen_embeddings,sentences_embeddings)
+
+        with open('knesset_similar_sentences.txt','w',encoding='utf-8') as file:
+         
+            for i,(orignal_sentence,tokenized_sentence) in enumerate(choosen_sentneces):
+                file.write(f"{orignal_sentence}: most similar sentences: ")
+
+                most_similar_idx=similarty_matrix[i].argmax()
+                most_similar_sentence=corpus_sentences[most_similar_idx]
+                # check if we get to the last sentence
+                if (i!=10):
+                    file.write(f"{most_similar_sentence}\n")    
+                else:
+                    file.write(f"{most_similar_sentence}")  
+
+    except Exception as e :
+        raise e
+      
+
+
+
+
 
 #%% main
 if __name__=='__main__':
@@ -113,11 +155,14 @@ if __name__=='__main__':
         ###########################################################################################################################################
         # section 1:1
         toknes=tokenize_sentences(corpus_sentences)
-        #print(toknes)
+        filtered_toknes=[]
+        for sentence in toknes:
+            filtered_toknes.append(toknes[sentence])
+        
 
         # section 1:2 
         # build the word2vec model
-        model=Word2Vec(sentences=toknes,vector_size=50,window=50,min_count=1)
+        model=Word2Vec(sentences=filtered_toknes,vector_size=50,window=5,min_count=1)
 
         # save the trained model
         model.save('knesset_word2vec.model')
@@ -131,14 +176,26 @@ if __name__=='__main__':
             'ישראל','גברת','ממשלה','חבר','בוקר','מים','אסור','רשות','זכויות'
         ]
         # to do : fix the path for the output 
+        ## section 2:1 - get the most 5 similar words for each word in the words list 
         similar_words=get_similar_words(words_list,word_vectors)
+
+        ## section 2:2 sentence embeddings 
+        ## check what we should send as input to the function 
+        sentences_embeddings=get_sentence_embeddings(filtered_toknes,word_vectors)
        
-             
+        #print(sentences_embeddings[0])   
 
 
+        # section 2:3 - get similar sentences
+        ## to do : choose the sentnece not randomly !!!!
+        valid_sentences=[(orignal_sentences,toknes) for orignal_sentences,toknes in toknes.items() if len(toknes)>=4]
+        choosen_sentneces=random.sample(valid_sentences,min(10,len(valid_sentences)))
+
+        
+        # to do : fix the path for the output 
+        get_similar_sentences(choosen_sentneces,sentences_embeddings,word_vectors,toknes,output_dir='output') 
    
     except Exception as e:
         raise e
     
-
 #%%
