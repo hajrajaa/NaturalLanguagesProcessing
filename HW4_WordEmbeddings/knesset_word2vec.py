43698a4978e15d3bb124972e8735320053f1cfb1
hajrajaa <129968202+hajrajaa@users.noreply.github.com>
2025-01-14 14:12:52 +0200

    start of section 2:4 : replace red words

--- HW4_WordEmbeddings/knesset_word2vec.py
+++ HW4_WordEmbeddings/knesset_word2vec.py
@@ -136,6 +136,42 @@ def get_similar_sentences(choosen_sentneces,sentences_embeddings,word_vectors,to
         raise e
       
 
+############################################################################################
+#section 4 - replace red words
+def replace_red_words(word_vectors,red_words_mapping,output_dir):
+    output_path=os.path.join(output_dir,'red_words_sentences.txt')
+    os.makedirs(output_dir,exist_ok=True)
+    replaced_words=[]
+    try:
+        with open('red_words_sentences.txt','w',encoding='utf-8') as file:
+
+            for i,(red_words,sentence) in  enumerate(red_words_mapping.items()):
+                updated_sentence=sentence
+                curr_replacement=[]
+                for red_word in red_words:
+                    if red_word in word_vectors.key_to_index.keys():
+                        similar_word=word_vectors.most_similar(red_word,topn=1)[0][0]
+                        updated_sentence=updated_sentence.replace(red_word,similar_word)
+                        curr_replacement.append((red_word,similar_word))
+                file.write(f"{i+1}: ")
+                file.write(f"{sentence}: {updated_sentence}\n")
+                file.write(f"replaced words: ")
+                for i,(red_word,similar_word) in  enumerate(curr_replacement):
+                    file.write(f"({red_word},{similar_word})")
+                    if i<len(curr_replacement)-1:
+                        file.write(',')
+                file.write('\n')
+                
+
+            replaced_words.extend(curr_replacement)
+
+
+    except Exception as e :
+        raise e
+    
+    return replaced_words
+    
+
 
 
 
@@ -150,7 +186,7 @@ if __name__=='__main__':
         corpus=load_corpus(input_path) 
         
         corpus_sentences=[sentence.get('sentence_text','') for sentence in corpus]
-        #corpus_sentences=corpus_sentences[:10]
+        
 
         ###########################################################################################################################################
         # section 1:1
@@ -158,6 +194,8 @@ if __name__=='__main__':
         filtered_toknes=[]
         for sentence in toknes:
             filtered_toknes.append(toknes[sentence])
+
+        
         
 
         # section 1:2 
@@ -169,31 +207,52 @@ if __name__=='__main__':
 
         #section 1:3 
         word_vectors=model.wv
-        print(word_vectors['ישראל'])   # get the vector of the word 
-        ##################################################################################################################################################
-        # section2 - similarity between words 
-        words_list=[
-            'ישראל','גברת','ממשלה','חבר','בוקר','מים','אסור','רשות','זכויות'
-        ]
-        # to do : fix the path for the output 
-        ## section 2:1 - get the most 5 similar words for each word in the words list 
-        similar_words=get_similar_words(words_list,word_vectors)
+        # print(word_vectors['ישראל'])   # get the vector of the word 
+        # ##################################################################################################################################################
+        # # section2 - similarity between words 
+        # words_list=[
+        #     'ישראל','גברת','ממשלה','חבר','בוקר','מים','אסור','רשות','זכויות'
+        # ]
+        # # to do : fix the path for the output 
+        # ## section 2:1 - get the most 5 similar words for each word in the words list 
+        # similar_words=get_similar_words(words_list,word_vectors)
 
-        ## section 2:2 sentence embeddings 
-        ## check what we should send as input to the function 
-        sentences_embeddings=get_sentence_embeddings(filtered_toknes,word_vectors)
+        # ## section 2:2 sentence embeddings 
+        # ## check what we should send as input to the function 
+        # sentences_embeddings=get_sentence_embeddings(filtered_toknes,word_vectors)
        
         #print(sentences_embeddings[0])   
 
 
         # section 2:3 - get similar sentences
         ## to do : choose the sentnece not randomly !!!!
-        valid_sentences=[(orignal_sentences,toknes) for orignal_sentences,toknes in toknes.items() if len(toknes)>=4]
-        choosen_sentneces=random.sample(valid_sentences,min(10,len(valid_sentences)))
+        # valid_sentences=[(orignal_sentences,toknes) for orignal_sentences,toknes in toknes.items() if len(toknes)>=4]
+        # choosen_sentneces=random.sample(valid_sentences,min(10,len(valid_sentences)))
 
         
-        # to do : fix the path for the output 
-        get_similar_sentences(choosen_sentneces,sentences_embeddings,word_vectors,toknes,output_dir='output') 
+        # # to do : fix the path for the output 
+        # get_similar_sentences(choosen_sentneces,sentences_embeddings,word_vectors,toknes,output_dir='output') 
+
+
+        ## section 4 : replace red words 
+        # sentences_with_red_words=[
+        #     ".בעוד מספר דקות נתחיל את הדיון בנושא השבת החטופים",
+        #     "בתור יושבת ראש הוועדה , אני מוכנה להאריך את ההסכם באותם תנאים.",
+        #     "בוקר טוב,אני פותח את הישיבה.",
+        #     "שלום , אנחנו שמחים להודיע שחברינו היקר קיבל קידום.",
+        #     "אין מניעה להמשיך לעסוק בנושא."
+        # ]
+
+        red_words_mapping={
+        ("דקות","הדיון"):".בעוד מספר דקות נתחיל את הדיון בנושא השבת החטופים",
+        ("הוועדה","אני","ההסכם"):"בתור יושבת ראש הוועדה , אני מוכנה להאריך את ההסכם באותם תנאים.",
+        ("בוקר","פותח"):"בוקר טוב , אני פותח את הישיבה.",
+        ("שלום","שמחים","היקר","קידום"):"שלום , אנחנו שמחים להודיע שחברינו היקר קיבל קידום.",
+        ("מניעה",):"אין מניעה להמשיך לעסוק בנושא."
+
+        }
+
+        x=replace_red_words(word_vectors,red_words_mapping,output_dir='output')
    
     except Exception as e:
         raise e
